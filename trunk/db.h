cbdb284678eec2324f1e86f4b2d96a9642e10e04
robert.hahn <robert.hahn@6e37a648-da27-0410-9c43-abe7cb931329>
2007-02-09 16:58:43 +0000

    Some minor changes.

--- trunk/db.h
+++ trunk/db.h
@@ -8,20 +8,14 @@
 /* vsf_db_open()
  * PURPOSE
  * Opens the database file. If any error occurs the process terminates.
- * PARAMETERS
- * RETURNS
  */
 void vsf_db_open();
-
 
 /* vsf_db_close()
  * PURPOSE
  * Closes the database file.
- * PARAMETERS
- * RETURNS
  */
 void vsf_db_close();
-
 
 /* vsf_db_check_auth()
  * PURPOSE
@@ -38,7 +32,6 @@ int vsf_db_check_auth(struct vsf_session* p_sess,
                       const struct mystr* p_user_str,
                       const struct mystr* p_pass_str,
                       const struct mystr* p_remote_host);
-
 
 /* vsf_db_log()
  * PURPOSE
@@ -113,21 +106,61 @@ int vsf_db_check_file(const struct vsf_session* p_sess,
                       const struct mystr* p_filename_str,
                       enum EVSFFileAccess what);
 
-
+/* vsf_db_change_password()
+ * PURPOSE
+ * Changes the password of the given user in the database.
+ * PARAMETERS
+ * p_sess          - the current session
+ * p_user_str      - user name
+ * p_pass_str      - new password
+ * RETURNS
+ * 1 if the password was changed, otherwise 0.
+ */
 int vsf_db_change_password(const struct vsf_session* p_sess,
                            const struct mystr* p_user_str,
                            const struct mystr* p_pass_str);
-                                                                      
+                                                                                                      
+/* vsf_db_check_credit()
+ * PURPOSE
+ * Checks the user has enough credit for a requested download.
+ * PARAMETERS
+ * p_sess          - the current session
+ * p_filename_str  - the path used to detect the section
+ * amount          - required credit in bytes
+ * RETURNS
+ * 1 if the user has enough credit, otherwise 0.
+ */
 int vsf_db_check_credit(const struct vsf_session* p_sess, 
                         const struct mystr* p_filename_str,
                         const filesize_t amount);
                         
+/* vsf_db_update_credit()
+ * PURPOSE
+ * Increases or decreases the credit of the current user.
+ * PARAMETERS
+ * p_sess          - the current session
+ * p_filename_str  - the path used to detect the section
+ * upload          - 1 = upload (inc credit), 0 = download (dec credit)
+ * amount          - credit delta in bytes
+ * RETURNS
+ * 1 if the update was successful, otherwise 0.
+ */
 int vsf_db_update_credit(const struct vsf_session* p_sess, 
                          const struct mystr* p_filename_str,
                          const int upload,
                          const filesize_t amount);
 
-int vsf_db_get_infoline(const struct vsf_session* p_sess,
-                        const struct mystr* p_dirname_str,
-                        struct mystr* infoline_str);
+/* vsf_db_get_infoline()
+ * PURPOSE
+ * Creates a string with section, credit and ratio information which is shown
+ * at the end of a directory listing.
+ * PARAMETERS
+ * p_sess          - the current session
+ * p_dirname_str   - the path used to detect the section
+ * p_infoline_str  - result (output variable)
+ * RETURNS
+ */
+void vsf_db_get_infoline(const struct vsf_session* p_sess,
+                         const struct mystr* p_dir_name_str,
+                         struct mystr* infoline_str);
 #endif /* VSF_DB_H */
